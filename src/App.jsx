121a1b3d0a13f2080aa042f78a23178659ba3ec6
William Alexander <williamax2005@gmail.com>
2025-05-19 19:29:26 +0700

    setup routing for each feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import ManajemenProdukRoutes from './features/manajemen-produk/ManajemenProdukRoutes.jsx'
+import ManajemenPelangganRoutes from './features/manajemen-pelanggan/ManajemenPelangganRoutes.jsx'
+import ManajemenPembayaranRoutes from './features/manajemen-pembayaran/ManajemenPembayaranRoutes.jsx'
+import ManajemenSupplierRoutes from './features/manajemen-supplier/ManajemenSupplierRoutes.jsx'
+import TransaksiPenjualanRoutes from './features/transaksi-penjualan/TransaksiPenjualanRoutes.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path='produk/' element={<ManajemenProdukRoutes />} />
+      <Route path='pelanggan/' element={<ManajemenPelangganRoutes />} />
+      <Route path='pembayaran/' element={<ManajemenPembayaranRoutes />} />
+      <Route path='supplier/' element={<ManajemenSupplierRoutes />} />
+      <Route path='transaksi/' element={<TransaksiPenjualanRoutes />} />
+    </Routes>
   )
 }
-
-export default App
